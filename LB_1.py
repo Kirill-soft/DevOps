40da1660e22f95230e6cff9a8f912aff21b5175e
Kirill-soft <nice.yakshin05@mail.ru>
2025-03-24 16:29:47 +0900

    first commit

---
+++ LB_1.py
@@ -0,0 +1,26 @@
+# Отрезок [1,4 ; 1,6]
+# Eps = 0,0001
+
+from math import cos, sin
+
+a = 1.4
+b = 1.6
+eps = 0.0001
+
+def f(x):
+	return (cos(x - 8)**2 - sin(x + 1)) / 2 - 0.2
+
+
+k = 0  # количество итераций цикла
+while b - a > 2*eps:
+	k += 1
+	c = (a + b) / 2  # середина отрезка
+	if f(a) * f(c) > 0:
+		a = c
+	else:
+		b = c
+
+x_vol = (a + b) / 2  # икс с волной
+print('X с волной:', x_vol)
+print('f(x_vol):', f(x_vol))
+print('Кол-во итераций:', k)
