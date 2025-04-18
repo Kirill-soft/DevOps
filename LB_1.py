44b7a1375f10174f497b72a52d11e7732c99dc2f
Kirill-soft <nice.yakshin05@mail.ru>
2025-04-18 19:19:43 +1000

    Pylint #2 haha

--- LB_1.py
+++ LB_1.py
@@ -7,20 +7,21 @@ a = 5.4
 b = 5.6
 eps = 0.0001
 
+
 def f(x):
-	return (cos(x - 8)**2 - sin(x + 1)) / 2 - 0.2
+    return (cos(x - 8) ** 2 - sin(x + 1)) / 2 - 0.2
 
 
 k = 0  # количество итераций цикла
-while b - a > 2*eps:
-	k += 1
-	c = (a + b) / 2  # середина отрезка
-	if f(a) * f(c) > 0:
-		a = c
-	else:
-		b = c
+while b - a > 2 * eps:
+    k += 1
+    c = (a + b) / 2  # середина отрезка
+    if f(a) * f(c) > 0:
+        a = c
+    else:
+        b = c
 
 x_vol = (a + b) / 2  # икс с волной
-print('X с волной:', x_vol)
-print('f(x_vol):', f(x_vol))
-print('Кол-во итераций:', k)
+print("X с волной:", x_vol)
+print("f(x_vol):", f(x_vol))
+print("Кол-во итераций:", k)
